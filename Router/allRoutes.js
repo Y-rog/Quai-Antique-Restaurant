d4ea2c46e790792f5da5a90041c2c8b7dcd80312
Y-rog <fulgueiras.gregory@gmail.com>
2024-06-07 18:17:33 +0200

    add gallery page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,8 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/home", "Accueil", "/pages/home.html")
+    new Route("/home", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
 ,];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
