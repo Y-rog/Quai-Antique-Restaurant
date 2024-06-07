47dd00860c31595a0eed3cc53ef27c0e59b72338
Y-rog <fulgueiras.gregory@gmail.com>
2024-06-07 19:00:12 +0200

    add account page and editPasword page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,10 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/home", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Modification de mot de passe", "/pages/auth/editPassword.html"),
 ,];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
