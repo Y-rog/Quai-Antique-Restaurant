8b5d266686ae5f79734b59f153f52d091c682b5d
Y-rog <fulgueiras.gregory@gmail.com>
2024-06-09 08:37:03 +0200

    add signup validate form

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,7 +6,7 @@ export const allRoutes = [
     new Route("/home", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Modification de mot de passe", "/pages/auth/editPassword.html"),
     new Route("/allBookings", "Vos réservations", "/pages/bookings/allBookings.html"),
