0f810ab1191cfdeb090da10224b43be8342f6a45
Y-rog <fulgueiras.gregory@gmail.com>
2024-06-09 07:46:20 +0200

    add edit Booking and see all bookings

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -9,6 +9,8 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Modification de mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allBookings", "Vos réservations", "/pages/bookings/allBookings.html"),
+    new Route("/toBook", "Réserver", "/pages/bookings/toBook.html"),
 ,];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
